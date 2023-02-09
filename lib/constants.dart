e6d7e8674cdd3a29b9a40f93c5c6ec15ab1db6f1
Julius Palalay <julius.palalay@usjr.edu.ph>
2023-02-09 21:18:07 +0800

    Second Commit (Allignment and Design)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 // clolors that we use in our app
-const kMainColor = Color(0xFF010F07);
-const kActiveColor = Color(0xFF22A45D);
+const kMainColor = Color.fromARGB(255, 6, 75, 36);
+const kActiveColor = Color.fromARGB(255, 30, 156, 87);
 const kAccentColor = Color(0xFFEF9920);
 const kBodyTextColor = Color(0xFF868686);
 const kInputColor = Color(0xFFFBFBFB);
